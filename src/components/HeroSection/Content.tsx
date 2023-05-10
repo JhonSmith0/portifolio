8117d0e8f4cbf4b35687d27a627d650376a8b667
TimboDeveloper <nathan.rochag@gmail.com>
2023-05-10 20:28:48 -0300

    ✨ feat: add cv button animation

--- src/components/HeroSection/Content.tsx
+++ src/components/HeroSection/Content.tsx
@@ -20,15 +20,48 @@ const SContent = styled.div`
     }
 
     .cv {
+      position: relative;
       background: none;
       outline: none;
       color: inherit;
       padding: 1.4rem 3.2rem;
       border: solid 1px #20c997;
+      color: #20c997;
+      border-radius: 3px;
       font-family: inherit;
       font-size: 1.8rem;
       margin-block: 2.4rem;
       cursor: pointer;
+
+      transition: color .3s, transform .3s cubic-bezier(.44,2,.19,.68);
+
+      span {
+        position: relative;
+        z-index: 2;
+      }
+
+      &::after {
+          content: '';
+          position:absolute;
+          display: block;
+          top: 0;
+          right: 0;
+          width: 0%;
+          transform: scale(1.0029);
+          height: 100%;
+          background: transparent;
+          transition: width .3s, background .3s;
+          z-index: 1;
+        }
+      &:hover {
+        color: #fff;
+        transform: scale(1.1);
+        &::after {
+          width: 100%;
+          left: 0;
+          background: #20c997;
+        }
+      }
     }
   }
 `;
